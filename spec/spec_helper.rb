916603c4bd864fe1ca53f18f3a99f27edbfa31c8
Adam Sven Johnson <adam@pkqk.net>
2014-10-13 16:35:35 +0100

    Use webmock instead of fakeweb
    VCR support for fakeweb is deprecated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ require 'odlifier'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.default_cassette_options = { :record => :once }
-  c.hook_into :fakeweb
+  c.hook_into :webmock
   c.configure_rspec_metadata!
 end
 
